fcd7814a612689457843b9b700d9d0aa0b93897e
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2023-08-21 18:47:43 +0300

    DZ2

--- Homework/DZ2/DZ2_01.py
+++ Homework/DZ2/DZ2_01.py
@@ -5,9 +5,25 @@
 '''
 
 num = int(input("Введите целое число: "))
+s = (f"Шестнадцитиричное представление числа {num}: ")
+assignment_table = {0: '0', 1: '1', 2: '2', 3: '3',
+                    4: '4', 5: '5', 6: '6', 7: '7',
+                    8: '8', 9: '9', 10: 'A', 11: 'B',
+                    12: 'C', 13: 'D', 14: 'E', 15: 'F'}
 
 
-# Проверка
-str_hex = hex(num)[2:]
-s = "Шестнадцитиричное представление: "
-print(s, str_hex)
+# Вариаант 1 -> рекурсивный
+def decimalToHexadecimal(num):
+    if (num <= 0):
+        return ''
+    remainder = num % 16
+    return decimalToHexadecimal(num//16) + assignment_table[remainder]
+
+
+print(s, decimalToHexadecimal(num))
+
+# Вариант 2 -> использование спецификатора формата
+print(s, '{0:x}'.format(num))
+
+# Проверка с помощью функции hex
+print(s, hex(num)[2:])
--- Homework/DZ2/DZ2_02.py
+++ Homework/DZ2/DZ2_02.py
@@ -6,26 +6,52 @@
 from fractions import Fraction
 
 
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def simplify_fraction(num, den):
+    greatest_common_divisor = gcd(num, den)
+    return num // greatest_common_divisor, den // greatest_common_divisor
+
+
+def add_fractions(num1, den1, num2, den2):
+    common_denominator = den1 * den2
+    sum_num = num1 * den2 + num2 * den1
+    return simplify_fraction(sum_num, common_denominator)
+
+
+def multiply_fractions(num1, den1, num2, den2):
+    return simplify_fraction(num1 * num2, den1 * den2)
+
+
 def sum_and_product(f1, f2) -> None:
-    frac1 = Fraction(f1)
-    frac2 = Fraction(f2)
-    sum_frac = frac1 + frac2
-    product_frac = frac1 * frac2
-    print("Сумма дробей:", sum_frac)
-    print("Произведение дробей:", product_frac)
+    num1, den1 = map(int, f1.split('/'))
+    num2, den2 = map(int, f2.split('/'))
+
+    sum_num, sum_den = add_fractions(num1, den1, num2, den2)
+    product_num, product_den = multiply_fractions(num1, den1, num2, den2)
+
+    print("Сумма дробей:", sum_num, "/", sum_den)
+    print("Произведение дробей:", product_num, "/", product_den)
 
 
+# Проверка
 def sum_and_product_verification(f1, f2) -> None:
     frac1 = Fraction(f1)
     frac2 = Fraction(f2)
     sum_frac = frac1 + frac2
     product_frac = frac1 * frac2
+    print("Проверка с помощью fractions:")
     print("Сумма дробей:", sum_frac)
     print("Произведение дробей:", product_frac)
 
 
 f1 = input("Введите первую дробь в формате a/b: ")
 f2 = input("Введите вторую дробь в формате a/b: ")
+
 
 sum_and_product(f1, f2)
 sum_and_product_verification(f1, f2)
