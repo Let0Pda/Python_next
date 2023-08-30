813d34e187c45ba511cd842dcb7a3ebba3c81896
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2023-08-30 16:30:02 +0300

    DZ3

--- Homework/DZ3/DZ3_03.py
+++ Homework/DZ3/DZ3_03.py
@@ -9,16 +9,28 @@
 
 
 def backpack(items, max_weight):
+    """
+    Рекурсивно находит все возможные комбинации предметов для рюкзака.
+
+    Args:
+        items (list): Список предметов, каждый элемент - кортеж (предмет, вес).
+        max_weight (float): Максимальный допустимый вес рюкзака.
+
+    Returns:
+        list: Список списков, представляющих возможные комбинации предметов.
+    """
     if not items or max_weight == 0:
         return [[]]
 
     item, weight = items[0]
     rest_items = items[1:]
 
+    # Получаем комбинации без текущего предмета
     combinations_without_item = backpack(rest_items, max_weight)
 
     combinations_with_item = []
     if weight <= max_weight:
+        # Получаем комбинации с текущим предметом
         combinations_with_item = backpack(
             rest_items, max_weight - weight)
         for combination in combinations_with_item:
@@ -28,6 +40,9 @@ def backpack(items, max_weight):
 
 
 def stuffing():
+    """
+    Основная функция для запуска программы подбора предметов в рюкзак.
+    """
     items = {
         'Спальник': 4,
         'Палатка': 7,
@@ -38,7 +53,6 @@ def stuffing():
         'Фонарь': 0.5,
         'Компас': 0.2,
         'Посуда': 0.6
-
     }
 
     max_weight = float(
@@ -56,4 +70,5 @@ def stuffing():
             print(f"{i}. {combination} (Вес рюкзака: {total_weight})")
 
 
+# Запускаем основную часть программы
 stuffing()
