e3def9077d3bed794829e61847182cf97af0463f
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2023-08-30 13:28:16 +0300

    DZ3

--- Homework/DZ3/DZ3_01.py
+++ Homework/DZ3/DZ3_01.py
@@ -3,3 +3,18 @@
 Вернуть список с дублирующимися элементами.
 В результирующем списке не должно быть дубликатов.
 '''
+
+
+def remove_duplicates(input_list):
+    result = []
+    for item in input_list:
+        if input_list.count(item) > 1 and item not in result:
+            result.append(item)
+    return result
+
+
+# Пример использования
+input_list = [1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 8,
+              9, 10, 10, 11, 12, 12, 12, 13, 14, 14, 15, 15]
+duplicates_list = remove_duplicates(input_list)
+print(duplicates_list)
--- Homework/DZ3/DZ3_02.py
+++ Homework/DZ3/DZ3_02.py
@@ -3,3 +3,35 @@
 10 самых частых. Не учитывать знаки препинания и регистр символов.
 За основу возьмите любую статью из википедии или из документации к языку.
 '''
+import re
+from collections import Counter
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_text_from_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup.get_text()
+
+
+def get_most_common_words(text, num_words=10):
+    cleaned_text = re.sub(r'[^\w\s]', '', text.lower())
+    words = cleaned_text.split()
+    word_counts = Counter(words)
+    return word_counts.most_common(num_words)
+
+
+# Пример URL (замените на нужный вам)
+sample_url = "https://ru.wikipedia.org/wiki/GeekBrains"
+
+# Получение текста со страницы по указанной ссылке
+sample_text = get_text_from_url(sample_url)
+
+# Получение 10 наиболее часто встречающихся слов
+most_common_words = get_most_common_words(sample_text, num_words=10)
+
+# Вывод результатов
+print('10 самых часто встречаемых слов: ')
+for i, (word, count) in enumerate(most_common_words, start=1):
+    print(f"{i}. {word}: {count}")
