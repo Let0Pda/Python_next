d0c43b13e0dcbeaf212ff0ab168f81f9e7195ae2
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2023-09-12 21:43:12 +0300

    Seminar4

--- Seminars/Seminar4/task07.py
+++ Seminars/Seminar4/task07.py
@@ -4,3 +4,35 @@
 Вычислите итоговую прибыль или убыток каждой компании. Если все компании
 прибыльные, верните истину, а если хотя бы одна убыточная — ложь.
 '''
+
+
+def calculate_profit(company_data):
+    profit_loss = {}
+    all_profitable = True
+
+    for company, financials in company_data.items():
+        revenue = sum(financials)
+        expenses = abs(sum(x for x in financials if x < 0))
+        profit = revenue - expenses
+        profit_loss[company] = profit
+
+        if profit < 0:
+            all_profitable = False
+
+    return profit_loss, all_profitable
+
+
+# Пример использования функции:
+company_data = {
+    "Company A": [10000, -2000, 3000],
+    "Company B": [-15000, 2000, 4000],
+    "Company C": [12000, -3000, -2500],
+    "Company D": [2000, -3000, -2500],
+}
+
+profits, all_profitable = calculate_profit(company_data)
+for company, profit in profits.items():
+    print(f"{company}: {profit} прибыль" if profit >=
+          0 else f"{company}: {profit} убыток")
+
+print("Все компании прибыльные" if all_profitable else "Есть убыточные компании")  # noqa
--- Seminars/Seminar4/task08.py
+++ Seminars/Seminar4/task08.py
@@ -4,3 +4,27 @@
 оканчивающихся на s (кроме переменной из одной буквы s) на None.
 Значения не удаляются, а помещаются в одноимённые переменные без s на конце.
 '''
+
+
+def remove_s(string):
+    # if len(string) > 1 and string[-1] == "s":
+    #     return None
+    # else:
+    #     return string
+    return None if len(string) > 1 and string[-1] == "s" else string
+
+
+a = 'strings'
+b = 'test'
+c = 'tes'
+s = 's'
+t = 'st'
+print(f'\nДо замены:\n{a = }\n{b = }\n{c = }\n{s = }\n{t = }')
+
+a = remove_s(a)
+b = remove_s(b)
+c = remove_s(c)
+s = remove_s(s)
+t = remove_s(t)
+
+print(f'\nПосле замены:\n{a = }\n{b = }\n{c = }\n{s = }\n{t = }')
