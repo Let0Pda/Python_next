4991299266899adde11160db4bb55f25577a8d40
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2023-09-13 18:10:34 +0300

    DZ5

--- Homework/DZ5/DZ5_3.py
+++ Homework/DZ5/DZ5_3.py
@@ -2,15 +2,35 @@
 Создайте функцию генератор чисел Фибоначчи (см. Википедию).
 '''
 
+#  Бесконечно генерит числа Фибоначчи при каждом вызове
 
-def fibonacci_generator():
+
+def fibonacci_gen():
     a, b = 1, 1
     while True:
         yield a
         a, b = b, a + b
 
 
-n = 10
-fib_gen = fibonacci_generator()
+n = 20
+fib_gen = fibonacci_gen()
 for _ in range(n):
     print(next(fib_gen), end=' ')
+
+print()
+
+# Через условие выхода из цикла 'while'
+
+
+def fibonacci_gen(limit):
+    a, b = 1, 1
+    count = 0
+    while count < limit:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+
+fib_gena = fibonacci_gen(n)
+for number in fib_gena:
+    print(number, end=' ')
