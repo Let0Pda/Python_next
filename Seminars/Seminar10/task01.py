899961a7a7677df62ecd9cfeed3c57e250a62556
Alexey Gavrilov <108893884+Let0Pda@users.noreply.github.com>
2023-10-16 22:55:23 +0300

    Seminar10

--- Seminars/Seminar10/task01.py
+++ Seminars/Seminar10/task01.py
@@ -1,7 +1,7 @@
-'''
+"""
 Создайте класс окружность. Класс должен принимать радиус окружности при создании экземпляра.
 У класса должно быть два метода, возвращающие длину окружности и её площадь.
-'''
+"""
 from math import pi
 
 
@@ -13,14 +13,18 @@ class Circle:
         return 2 * pi * self.radius
 
     def cir_rad(self):
-        return pi * self.radius ** 2
+        return pi * self.radius**2
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cir1 = Circle(4)
     cir2 = Circle(6)
     cir3 = Circle(8)
-    print(f'Длинна окружности:\n cir1 = {round(cir1.circ_len(), 2)}\n cir2 = {round(cir2.circ_len(), 2)}\
-        \n cir3 = {round(cir3.circ_len(), 2)}')
-    print(f'Площадь окружности:\n cir1 = {round(cir1.cir_rad(), 2)}\n cir2 = {round(cir2.cir_rad(), 2)}\
-        \n cir3 = {round(cir3.cir_rad(), 2)}')
+    print(
+        f"Длинна окружности:\n cir1 = {round(cir1.circ_len(), 2)}\n cir2 = {round(cir2.circ_len(), 2)}\
+        \n cir3 = {round(cir3.circ_len(), 2)}"
+    )
+    print(
+        f"Площадь окружности:\n cir1 = {round(cir1.cir_rad(), 2)}\n cir2 = {round(cir2.cir_rad(), 2)}\
+        \n cir3 = {round(cir3.cir_rad(), 2)}"
+    )
